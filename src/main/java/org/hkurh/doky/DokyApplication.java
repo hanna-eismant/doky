ec03203404a3604fb6dd7baf4262e3b69b3633bb
Hanna Kurhuzenkava <hanna.kurhuzenkava@outlook.com>
2022-01-04 00:03:45 +0300

    Setup Project in IDE

---
+++ src/main/java/org/hkurh/doky/DokyApplication.java
@@ -0,0 +1,16 @@
+package org.hkurh.doky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DokyApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(DokyApplication.class, args);
+	}
+
+}
