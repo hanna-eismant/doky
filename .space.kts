1a362d71f87a3b58d4785d80a471e6bd7c57728b
Hanna Kurhuzenkava <hanna.kurhuzenkava@outlook.com>
2023-03-03 18:19:10 +0000

    New file .space.kts

---
+++ .space.kts
@@ -0,0 +1,9 @@
+/**
+* JetBrains Space Automation
+* This Kotlin-script file lets you automate build activities
+* For more info, see https://www.jetbrains.com/help/space/automation.html
+*/
+
+job("Build") {
+   gradlew("amazoncorretto:11-alpine", "build")
+}
