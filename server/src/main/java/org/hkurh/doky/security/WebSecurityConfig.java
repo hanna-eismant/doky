0871c9ed3e268716263b9e78e31349adb17d456b
Anton <kurguzenkov@gmail.com>
2023-03-04 11:24:37 +0300

    feat: configure cors (#38)

--- server/src/main/java/org/hkurh/doky/security/WebSecurityConfig.java
+++ server/src/main/java/org/hkurh/doky/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package org.hkurh.doky.security;
 
 import org.hkurh.doky.filters.JwtAuthorizationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +10,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @EnableWebSecurity
@@ -25,7 +29,16 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.httpBasic().disable()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .addFilterAfter(getJwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+                .addFilterAfter(getJwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .cors();
+    }
+
+    @Bean
+    protected CorsConfigurationSource corsConfigurationSource() {
+        final var source = new UrlBasedCorsConfigurationSource();
+        final var corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
     }
 
     private JwtAuthorizationFilter getJwtAuthorizationFilter() {
