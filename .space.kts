27983cc690aa2ba60216511d4bd6b9ee867ff4f6
Hanna Kurhuzenkava <hanna.kurhuzenkava@outlook.com>
2023-04-14 18:34:35 +0000

    MAIN-T-18-Configure-deploy-on-DEV-Azure

--- .space.kts
+++ .space.kts
@@ -33,3 +33,42 @@ job("API Tests") {
         }
     }
 }
+
+job("Azure DEV Deployment") {
+    startOn {
+        gitPush {
+            branchFilter {
+                +"refs/heads/develop"
+            }
+        }
+    }
+    parameters {
+        text("spring-profile", value = "dev")
+    }
+
+    container(displayName = "Deploy artifact", image = "gradle:6.9.0-jdk11") {
+        workDir = "server"
+
+        env["AZURE_SUBSCRIPTION"] = "{{ project:azure-subscription }}"
+        env["AZURE_RESOURCE_GROUP"] = "{{ project:azure-resource-group }}"
+        env["AZURE_APP_NAME"] = "{{ project:azure-app-name }}"
+        env["AZURE_TENANT"] = "{{ project:azure-tenant }}"
+        env["AZURE_CLIENT"] = "{{ project:azure-client }}"
+        env["AZURE_AUTH_KEY"] = "{{ project:azure-auth-key }}"
+
+        env["SPRING_PROFILES_ACTIVE"] = "{{ spring-profile }}"
+        env["SPRING_DATASOURCE_URL"] = "{{ project:spring-datasource-url }}"
+        env["SPRING_DATASOURCE_USERNAME"] = "{{ project:spring-datasource-username }}"
+        env["SPRING_DATASOURCE_PASSWORD"] = "{{ project:spring-datasource-password }}"
+
+        kotlinScript { api ->
+            api.space().projects.automation.deployments.start(
+                    project = api.projectIdentifier(),
+                    targetIdentifier = TargetIdentifier.Key("azure-dev"),
+                    version = "Aardvark-v0.1." + api.executionNumber(),
+                    syncWithAutomationJob = true
+            )
+            api.gradle("azureWebAppDeploy")
+        }
+    }
+}
