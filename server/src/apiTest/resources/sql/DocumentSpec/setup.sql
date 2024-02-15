22b9df0deee0d1c9b3697e3f5bb4946979535139
Hanna Kurhuzenkava <hanna_kurhuzenkava@epam.com>
2024-02-15 20:44:50 +0300

    MAIN-T-47 Add changes history for Document

--- server/src/apiTest/resources/sql/DocumentSpec/setup.sql
+++ server/src/apiTest/resources/sql/DocumentSpec/setup.sql
@@ -1,13 +1,13 @@
 insert into document (name, description, created_date, modified_date, creator_id  )
-select "Test_1", "That is a test document", 0, 0, u.id from user u where u.uid = "hanna_test_1";
+select "Test_1", "That is a test document", '2024-01-15 13:00:00', '2024-01-15 13:00:00', u.id from user u where u.uid = "hanna_test_1";
 
 insert into document (name, description, created_date, modified_date, creator_id )
-select "Test_2", "That is a second test document", 0, 0, u.id from user u where u.uid = "hanna_test_1";
+select "Test_2", "That is a second test document", '2024-01-15 16:00:00', '2024-01-15 16:00:00', u.id from user u where u.uid = "hanna_test_1";
 
 insert into user (uid, name, password) values ( "hanna_test_2", "Hanna", "$2a$10$bdZSuBncZqaM4XwHcjxbpeQuXeOxk6vCEsFrTwa91xh3M3JpvW41m" );
 
 insert into document (name, description, created_date, modified_date, creator_id )
-select "Test_3", "That is a test document", 0, 0, u.id from user u where u.uid = "hanna_test_2";
+select "Test_3", "That is a test document", '2024-02-13 13:00:00', '2024-03-15 13:24:00', u.id from user u where u.uid = "hanna_test_2";
 
 insert into document (name, description, created_date, modified_date, creator_id )
-select "Test_4", "That is a second test document", 0, 0, u.id from user u where u.uid = "hanna_test_2";
+select "Test_4", "That is a second test document", '2024-01-15 13:00:00', '2024-01-15 13:00:00', u.id from user u where u.uid = "hanna_test_2";
