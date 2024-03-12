3dae65dfd293669793ba9569a5fa33e2813f237e
Hanna Kurhuzenkava <hanna-eismant@users.noreply.github.com>
2024-03-12 16:50:59 +0300

    MAIN-T-63 Configure SMTP on dev env (#67)

--- server/src/main/java/org/hkurh/doky/email/EmailService.kt
+++ server/src/main/java/org/hkurh/doky/email/EmailService.kt
@@ -1,7 +1,23 @@
 package org.hkurh.doky.email
 
 import org.hkurh.doky.users.db.UserEntity
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.mail.SimpleMailMessage
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.stereotype.Service
 
-interface EmailService {
-    fun sendRegistrationConfirmationEmail(user: UserEntity)
+@Service
+class EmailService(private val emailSender: JavaMailSender) {
+
+    @Value("\${doky.email.from}")
+    private val fromEmail: String? = null
+
+    fun sendRegistrationConfirmationEmail(user: UserEntity) {
+        val message = SimpleMailMessage()
+        message.from = fromEmail
+        message.setTo(user.uid)
+        message.subject = "Doky Registration"
+        message.text = "Welcome! You registered a new user in Doky app."
+        emailSender.send(message)
+    }
 }
