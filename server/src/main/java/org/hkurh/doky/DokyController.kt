777c94389f6091cddcebe8ba896162184e4f431c
Hanna Kurhuzenkava <hanna-eismant@users.noreply.github.com>
2024-04-11 09:10:11 +0300

    MAIN-T-106 Add endpoint for `/` (#85)

--- server/src/main/java/org/hkurh/doky/DokyController.kt
+++ server/src/main/java/org/hkurh/doky/DokyController.kt
@@ -1,6 +1,8 @@
 package org.hkurh.doky
 
+import com.azure.core.annotation.Get
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -9,6 +11,11 @@ class DokyController {
 
     @Value("\${doky.version.build}")
     lateinit var build: String
+
+    @GetMapping("/")
+    fun root() : ResponseEntity<*> {
+        return ResponseEntity.ok().body("OK")
+    }
 
     @GetMapping("/version")
     fun version(): VersionDto {
