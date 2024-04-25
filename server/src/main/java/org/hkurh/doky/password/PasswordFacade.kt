9a81750a02561c99df473936ea5870ebf4674862
Hanna Kurhuzenkava <hanna_kurhuzenkava@epam.com>
2024-04-25 14:57:58 +0300

    Add logging and exception handling to reset password process
    In the PasswordFacade class, a log statement and exception handling have been added to the reset password function. Now, when the password reset email is sent, it's wrapped in a try/catch block to handle any potential exceptions. Additionally, a debug-level log is generated whenever a rest password token is created.

--- server/src/main/java/org/hkurh/doky/password/PasswordFacade.kt
+++ server/src/main/java/org/hkurh/doky/password/PasswordFacade.kt
@@ -3,6 +3,7 @@ package org.hkurh.doky.password
 import org.hkurh.doky.email.EmailService
 import org.hkurh.doky.errorhandling.DokyNotFoundException
 import org.hkurh.doky.users.UserService
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
@@ -13,10 +14,20 @@ class PasswordFacade(
 ) {
 
     fun reset(email: String) {
-        if (!userService.exists(email)) throw DokyNotFoundException("User with email $email does not exist")
+        if (!userService.exists(email)) throw DokyNotFoundException("User does not exist")
 
         val user = userService.findUserByUid(email)
         val token = resetPasswordService.generateAndSaveResetToken(user!!)
-        emailService.sendRestorePasswordEmail(user, token)
+        LOG.debug("Generate reset password token for user ${user.id}")
+        try {
+            emailService.sendRestorePasswordEmail(user, token)
+        } catch (e: Exception) {
+            LOG.error("Error sending reset password email for user ${user.id}", e)
+        }
+    }
+
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(PasswordFacade::class.java)
     }
 }
