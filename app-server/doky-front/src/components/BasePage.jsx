322baf02ce23d7edd681c9e5d52b4594aaa118b5
Hanna Kurhuzenkava <hanna.kurhuzenkava@outlook.com>
2024-10-01 23:01:49 +0300

    DOKY-150 Update version badge URL to use current backend endpoint (#121)
    Remove version badge as was decided that it is not necessary.
    Update `.gitignore` at project root. Fix path in `husky` pre-commit hook.
    Fix husky path in npm prepare script
    
    ---------
    
    Co-authored-by: Anton <kurguzenkov@gmail.com>

--- app-server/doky-front/src/components/BasePage.jsx
+++ app-server/doky-front/src/components/BasePage.jsx
@@ -6,13 +6,7 @@ const BasePage = () => (
     <header className="navbar bg-primary text-white bg-gradient sticky-top">
       <div className="container-fluid">
         <Link className="navbar-brand text-white" to="/">
-          <img height="40px" src="/logo-white-no-bg.svg"/>
-          <span className="version-badge">
-            <img
-              alt="Endpoint Badge"
-              src="https://img.shields.io/endpoint?url=https%3A%2F%2Fserver.blackfield-1e13811b.westeurope.azurecontainerapps.io%2Fapi%2Fapp-version"
-            />
-          </span>
+          <img height="40px" src="/logo-white-no-bg.svg" alt="Doky Logo"/>
         </Link>
       </div>
     </header>
