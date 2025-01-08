536852a29b775cf88318e4ecd1fc9beb8ba7aae4
Hanna Kurhuzenkava <hanna.kurhuzenkava@outlook.com>
2025-01-08 13:12:50 +0300

    DOKY-204 Update deployment for `email-service` (#134)
    Integrate versioning, logging, and deployment updates
    
    Added unified versioning and build date across projects. Introduced JSON logging for production in `email-service` with `Logback`. Enhanced Docker configurations for both 'app-server' and 'email-service' with dynamic environment variables and deploy settings.

--- email-service/src/main/kotlin/org/hkurh/doky/kafka/KafkaEmailNotificationConsumerService.kt
+++ email-service/src/main/kotlin/org/hkurh/doky/kafka/KafkaEmailNotificationConsumerService.kt
@@ -24,7 +24,7 @@ class KafkaEmailNotificationConsumerService(
     )
     fun listen(@Payload message: SendEmailMessage) {
         try {
-            LOG.debug { "Received message: $message" }
+            LOG.debug { "Received message: [$message]" }
             message.userId.let {
                 when (message.emailType) {
                     EmailType.REGISTRATION -> sendRegistrationEmail(message.userId!!)
@@ -33,8 +33,7 @@ class KafkaEmailNotificationConsumerService(
                 }
             }
         } catch (e: Exception) {
-            LOG.error(e) { "Error processing message: $message" }
-            throw e
+            LOG.error(e) { "Error processing message: [$message]" }
         }
     }
 
