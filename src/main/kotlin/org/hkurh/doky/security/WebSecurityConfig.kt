fe0fd9aeb2658cd0807525ccb8dd5c50d777dee7
Hanna Kurhuzenkava <hanna.kurhuzenkava@outlook.com>
2025-03-17 14:48:53 +0300

    DOKY-235 Deploy front end app separately, restrict direct access to backend (#159)
    Remove Node plugin and frontend build integration.
    
    This commit removes the Gradle Node plugin and related frontend build configuration, simplifying the project by decoupling Node.js processes from the backend build. Additionally, unused configurations such as the `DokyWebConfig` class were deleted to clean up the codebase. Minor CORS and property updates were made to ensure smooth system behavior post-removal.

--- src/main/kotlin/org/hkurh/doky/security/WebSecurityConfig.kt
+++ src/main/kotlin/org/hkurh/doky/security/WebSecurityConfig.kt
@@ -66,7 +66,11 @@ internal class WebSecurityConfig(private val jwtAuthorizationFilter: JwtAuthoriz
     protected fun corsConfigurationSource(): CorsConfigurationSource {
         val source = UrlBasedCorsConfigurationSource()
         val corsConfiguration = CorsConfiguration().applyPermitDefaultValues()
+        corsConfiguration.addAllowedMethod(HttpMethod.OPTIONS)
+        corsConfiguration.addAllowedMethod(HttpMethod.GET)
+        corsConfiguration.addAllowedMethod(HttpMethod.POST)
         corsConfiguration.addAllowedMethod(HttpMethod.PUT)
+        corsConfiguration.addAllowedMethod(HttpMethod.HEAD)
         source.registerCorsConfiguration("/**", corsConfiguration)
         return source
     }
