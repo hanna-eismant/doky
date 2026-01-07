aee355e872c000b821f8a58c619112a6869f6aba
Hanna Kurhuzenkava <hanna.kurhuzenkava@outlook.com>
2026-01-07 19:04:47 +0300

    DOKY-201 Fix eslint error

--- doky-front/src/index.js
+++ doky-front/src/index.js
@@ -21,7 +21,7 @@ import React, {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import {createTheme, ThemeProvider} from '@mui/material/styles';
 import {CssBaseline} from '@mui/material';
-import { ErrorBoundary } from '@datadog/browser-rum-react'
+import { ErrorBoundary } from '@datadog/browser-rum-react';
 
 import './index.scss';
 
